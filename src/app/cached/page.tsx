11f9b6df36ed6f9d06366786b311555047a0e36c
greidinger.dev <jp@greidinger.dev>
2023-03-16 13:33:40 -0300

    cache test

---
+++ src/app/cached/page.tsx
@@ -0,0 +1,13 @@
+import { appRouter } from "~/server/api/root";
+import { db } from "~/server/db";
+
+export default async function CachedPage() {
+    const caller = appRouter.createCaller({ db, session: null });
+    const { lastUpdated } = await caller.public.slowQueryCached();
+
+    return (
+        <div>
+            Last Cache date = {new Date(lastUpdated).toLocaleDateString()}
+        </div>
+    );
+}
