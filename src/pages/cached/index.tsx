69648eca9485e3e9aa287cdd02247dfb57e3eafc
greidinger.dev <jp@greidinger.dev>
2023-03-16 16:04:03 -0300

    edgeruntime test #192381248

--- src/pages/cached/index.tsx
+++ src/pages/cached/index.tsx
@@ -1,33 +1,11 @@
-import { type GetServerSideProps } from "next"
-import { appRouter } from "~/server/api/root"
-import { createProxySSGHelpers } from "@trpc/react-query/ssg"
-import { createInnerTRPCContext } from "~/server/api/trpc"
-import superjson from "superjson"
 import { api } from "~/utils/api"
-
-export const getServerSideProps = (async () => {
-    const ssg = createProxySSGHelpers({
-        ctx: createInnerTRPCContext({ session: null }),
-        router: appRouter,
-        transformer: superjson,
-    })
-    await ssg.public.slowQueryCached.prefetch()
-
-    return {
-        props: {
-            trpcState: ssg.dehydrate(),
-        },
-    }
-}) satisfies GetServerSideProps
 
 export default function CachedPage() {
     const { data } = api.public.slowQueryCached.useQuery()
 
-    if (!data) return null
-
     return (
         <div>
-            Last Cache date = {new Date(data.lastUpdated).toLocaleTimeString()}
+            Last Cache date = {data && new Date(data.lastUpdated).toLocaleTimeString()}
         </div>
     )
 }
